939e9e0ffbe8c1696fdddd010a62f86affcf7b75
batch <lucashernadez904@gmail.com>
2023-11-13 16:45:16 -0300

    feat: consuming autg store from login

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,6 +6,7 @@ const userInfo = AuthStore.getUserInfo()
 export const Home = () => {
   return (
     <div>
+      <h1>Soy el home</h1>
       <span>{accessToken}</span>
       <br />
       <span>{userInfo && userInfo.name}</span>
